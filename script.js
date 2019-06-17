6bde5be1012672a059d96da0a70266a91c5fc8b0
rayremedi <ray.remedi17@laposte.net>
2019-06-17 15:09:32 +0200

    ajout de commentaires

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 
+//Tableau pour les titres.
 var titleArray = {
     Epaule: 'Epaule',
     Main: 'Main'
 }
-
+//Tableau pour les paragraphes.
 var paraArray = {
     Epaule: 'Ceci est une Epaule',
     Main: 'ceci est une Main'
 }
-//Fonction qui change le backgroundColor en vert
+//Fonction 1 qui change la couleur du background en vert, et fait apparaitre le titre des parties du corps, lorsque la souris est sur les divs.
 function bodyappearRed(id) {
     var monElt = document.getElementById(id);
     monElt.style.backgroundColor = 'green';
@@ -18,14 +19,14 @@ function bodyappearRed(id) {
     titleParts.innerHTML = titleArray[id];
     titleParts.style.display = "block";
 }
-
+//Fonction 3  qui fait apparaitre le paragraphe si on clique sur les div.
 function onClick(id) {
     var txtParts = document.getElementById('txt-para');
     txtParts.innerHTML = paraArray[id];
     console.log(paraArray[id]);
     txtParts.style.display = "block";
 }
-
+//Fonction 2 qui  fait disparaitre la couleur du background (vert) ainsi que le titre et le paragraphe sur les divs.
 function BodydisappearRed(id) {
     var monElt = document.getElementById(id);
     monElt.style.backgroundColor = '';
